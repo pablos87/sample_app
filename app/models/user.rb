4ee454c69d5713248ef829a5dbbc4cc97ad45b0d
Paweł <pawelbaraniewicz@gmail.com>
2011-03-29 19:46:04 +0200

    Added a relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,19 +13,6 @@
 #  admin              :boolean
 #
 
-# == Schema Information
-# Schema version: 20110326130235
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#  salt               :string(255)
-#
 require 'digest'
 
 class User < ActiveRecord::Base
@@ -33,7 +20,14 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation  
 	
   has_many :microposts, :dependent => :destroy
-  
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
   validates :name, :presence => true,
@@ -64,6 +58,18 @@ class User < ActiveRecord::Base
     (user && user.salt == cookie_salt) ? user : nil
   end
   
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
   def feed
     Micropost.where("user_id = ?", id)
   end
